7cf358b8fded676584eeb551a8ebfbf463bcd173
Geoff <eckertg1@yahoo.com>
2017-10-23 06:26:08 -0400

    checkpoint 8 simple dom 2: completed

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
-    event.preventDefault();
+  function createNewToDo(){
+    const newToDoText = document.getElementById('newToDoText');
+//I changed the if statement because !newToDoText didn't seem to be working
+//with regards to catching empty to-do entries.
+    if (newToDoText.value === '') { return }
 
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
     newToDoText.value = '';
+
+    renderTheUI(toDos);
+  }
+
+  function renderTheUI(toDos) {
+    const todoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo){
+        const newLi = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+
+        newLi.textContent = toDo.title;
+
+        todoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+    });
+
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
+
+  renderTheUI(toDos);
 }
 
 window.onload = function() {
